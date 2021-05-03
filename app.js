25ea252ae4223b9d95efb9e036e9f1c1ef66f30b
Andrew McMillion <amcmillion3@gmail.com>
2021-05-03 09:49:02 +0200

    Add color changing

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
 const colorBtn = document.getElementById("color-btn");
 const grayBtn = document.getElementById("gray-btn");
 const changeSizeBtn = document.getElementById("change-size-btn");
-const gridContainer = document.getElementById("grid-container");
+const sketchBody = document.querySelector(".sketch-body");
 
 const layoutGrid = (size) => {
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
-}
+    sketchBody.removeChild(sketchBody.lastChild);
+    let gridContainer = document.createElement("div");
+    gridContainer.classList.add("grid-container");
+    sketchBody.appendChild(gridContainer);
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for(i = 0; i < (size ** 2); i++) {
+        let gridDiv = document.createElement("div");
+        gridDiv.classList.add("grid-div");
+        gridContainer.appendChild(gridDiv);
+    };
+};
 
 const changeSize = () => {
     let size = window.prompt("What dimension grid would you like?");
-    layoutGrid(size);
-}
+    if( size <= 64 && size > 0 ) {
+        layoutGrid(size);
+    } else {
+        changeSize();
+    };
+};
 
 changeSizeBtn.addEventListener("click", () => {
     changeSize();
-})
+});
+
+colorBtn.addEventListener("click", () => {
+    let gridDiv = document.querySelectorAll(".grid-container > div");
+    for( let i = 0; i < gridDiv.length; i++) {
+        gridDiv[i].onmouseover = (e) => {
+            e.target.style.background = `${randomColor()}`;
+        };
+    };
+});
+
+const randomColor = function() {
+    let number = function() {
+        let randomNumber = Math.floor(Math.random() * 256);
+        return randomNumber;
+    };
+    return `rgb( ${number()}, ${number()}, ${number()})`;
+};
+
+document.addEventListener("DOMContentLoaded", layoutGrid(16));
